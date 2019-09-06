22da4a6db2f3f4c8907afe2c07c9240ddea54c71
Matt Ruehling <49105348+MattR246@users.noreply.github.com>
2019-09-06 13:37:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db = knex({
   	client: 'pg',
 	  connection: {
 	    host : '127.0.0.1',
-	    user : 'postgres',
-	    password : 'jx88SK#%',
+	    user : '********',
+	    password : '********',
 	    database : 'smartbrain'
   }
 });
